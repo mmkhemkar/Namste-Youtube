8a4e36d20302ac8dd15612a93a338dcc9ce4100f
Mayur Khemkar (Harman Connected ServicesCorporatio) <v-mkhemkar@microsoft.com>
2025-04-27 12:27:17 +0530

    refactored the code

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Comment from './Comment';
 
-const CommentList = () => {
-  return (
-    <div>CommentList</div>
-  )
+const CommentList = ({comments}) => {
+  return comments?.map((comment, i) => (
+    <div key={i}>
+      <Comment  data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <CommentList comments={comment.replies}/>
+      </div>
+    </div>
+  ));
 }
 
 export default CommentList
